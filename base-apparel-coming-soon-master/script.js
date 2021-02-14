6bffbc76e1576bee8e4e60803babb0ac92c9518b
Esesosa-max <75207794+Esesosa-max@users.noreply.github.com>
2021-02-14 23:25:02 +0100

    Add files via upload

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const error_icon = document.querySelector(".error-icon");
+const small = document.querySelector("small");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailVal = email.value;
+
+  // check if it is a valid email
+  if (!validateEmail(emailVal)) {
+    error_icon.style.display = "block";
+    small.style.display = "block";
+    e.preventDefault();
+  } else {
+    form.classList.remove("error");
+    document.querySelector(
+      "body"
+    ).innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1>Thank you!</h1></div>`;
+  }
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
